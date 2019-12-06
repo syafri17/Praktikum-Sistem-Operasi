7a766f21b3e309b9ef0e329f9ec84b8a7da98b2e
syafri17 <58585400+syafri17@users.noreply.github.com>
2019-12-06 17:04:05 +0700

    Update 3.sh

--- Legacy Codes/3.sh
+++ Legacy Codes/3.sh
@@ -1,84 +1,95 @@
 clear
-i=0
-declare -a nama
-declare -a npm
-declare -a jur
+tes=y
 
-while :;
+while [ $tes == 'y' ]
 do
- echo -e "Menu : \n\n1. Input\n2. View\n3. Search\n4. Exit\n"
- echo -n "Masukkan pilihan : "
- read pilih
+echo "== Program Nilai Terkecil Kedua di dalam Array =="
+echo "1. Demo Program Array"
+echo "2. Lihat Data Array"
+echo "3. Rata-rata Array"
+echo "4. Exit"
+echo "Pilihan :"
+read pil
 
- if (("$pilih" == 1));   # INPUT DATA
- then
-  clear
-  echo -n "Masukkan Nama    : "
-  read nama[$i]
-  echo -n "Masukkan NPM     : "
-  read npm[$i]
-  echo -n "Masukkan Jurusan : "
-  read jur[$i]
-  i=`expr $i + 1`
-  clear
+case $pil in
+1)
+echo "Masukkan banyak element array : "
+read input
 
- elif (("$pilih" == 2));   # TAMPILKAN DATA
- then
-  if (( i == 0))
-  then
-   clear
-   echo "Tidak ada data yang dapat ditampilkan"
-   read
-   clear
-  else
-   clear
-   echo -e "Data yang telah dimasukkan : \n"
-   for (( q=0; q<i;q++  ))
-   do
-    echo -e "Data ke $[q+1]\nNama    : ${nama[q]}\nNPM     : ${npm[q]}\nJurusan : ${jur[q]}\n"
-   done
-   read
-   clear
-  fi
+for((i=0;i<$input;i++))
+do
+    printf "Index array - $i : "
+    read bibit[i]
+done
 
- elif (("$pilih" == 3));   # CARI DATA
- then
-  if (( i == 0))
-  then
-   clear
-   echo "Tidak ada data yang dapat dicari"
-   read
-   clear
-  else
-   clear
-      echo -n "Masukkan NPM mahasiswa yang ingin dicari : "
-      read cari
+terkecil_prtm=${bibit[0]}
+for((i=0;i<$input;i++))
+do
+    if [[ $terkecil_prtm -gt ${bibit[i]} ]]
+    then
+        let terkecil_prtm=${bibit[i]}
+        let halah=i
+    fi
+done
 
-      k=0
-      while (($cari != ${npm[$k]}))
-      do
-    k=`expr $k + 1`
-      done
+terkecil_kedua=99999
+index=0
+for((i=0;i<$input;i++))
+do
+    if [[ $i -eq $halah ]]
+    then
+        let i=i+1
+        let i=i-1
+    else
+          if [[ $terkecil_kedua -gt ${bibit[i]} ]]
+        then
+let terkecil_kedua=${bibit[i]}
+            let index=i
+        fi
+    fi
+done
 
-   if (($cari == ${npm[$k]}));
-   then
-    echo -e "\nNama    : ${nama[k]}\nNPM     : ${npm[k]}\nJurusan : ${jur[k]}"
+printf "\n"
+echo "Nilai terkecil kedua di dalam array adalah : $terkecil_kedua"
+echo "Array terkecil kedua Index ke : $index"
+read
+clear
+;;
 
-   else
-    clear
-    echo -e "Data tidak ditemukan"
-   fi
-   read
-      clear
-  fi
+2)
+if [[ $input -eq 0 ]]
+then
+    printf "Data Masih Kosong \n"
+else
+    echo ${bibit[@]}
+fi
+read
+clear
+;;
 
- elif (( "$pilih" != 4 && "$pilih" != 2 && "$pilih" != 1 && "$pilih" !=       3  ));   # APABILA MEMBERIKAN MASUKAN SELAIN 1-4
- then
-  echo "Pilihan tidak valid"
-  read
-  clear
+3)
+if [[ $input -eq 0 ]]
+then
+    printf "\n"
+    printf "0\n"
+else
+    banyak_data=${#bibit[@]};
+    flag=0
+    for((i=0;i<$banyak_data;i++))
+    do
+        let flag=$(expr "$flag" + "${bibit[i]}")
+    done
 
- else    # KELUAR PROGAM
-  exit
- fi
+let tot=$flag/$banyak_data
+echo "Rata-rata data array : $tot"
+fi
+read
+clear
+;;
+
+4)
+printf "\n"
+echo "Terima kasih"
+exit 1
+esac
 done
